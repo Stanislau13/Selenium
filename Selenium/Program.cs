50000f47d09a2c8765bc9f7cc39ad35e0084913c
Stanislau Strechyn <stanislau.strechyn@gmail.com>
2023-02-06 00:33:44 +0300

    authorization

---
+++ Selenium/Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace Selenium;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        WebDriver driver = new ChromeDriver();
+
+        Authorization login = new Authorization (driver);
+
+        login.InputLoginAndPassword();
+
+        driver.Close();            
+    }
+}
