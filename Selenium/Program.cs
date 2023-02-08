c19151112e88f46206638c2dafe6e83d442b12c3
Stanislau Strechyn <stanislau.strechyn@gmail.com>
2023-02-08 14:23:32 +0300

    devbyio

--- Selenium/Program.cs
+++ Selenium/Program.cs
@@ -6,12 +6,21 @@ internal class Program
 {
     static void Main(string[] args)
     {
-
         WebDriver driver = new ChromeDriver();
+        Devbyio devBy = new Devbyio(driver);
 
-        Authorization login = new Authorization (driver);
+        string homePageJavaVacancyRateString = devBy.ExtractVacancies();
+        Console.WriteLine($"Java vacancies on the home page {homePageJavaVacancyRateString}");
 
-        login.InputLoginAndPassword();
+        devBy.LinkToTheJavaJobList();
+        int vacancyPagejavaVacancyRate = devBy.OutputDescriptionsOfAllJavaVacancies();
+        Console.WriteLine($"Java vacancies on the jobs page {vacancyPagejavaVacancyRate}");
+
+        Console.WriteLine($"!!!!!!!!!!! {homePageJavaVacancyRateString.Equals(vacancyPagejavaVacancyRate.ToString())}");
+        if (homePageJavaVacancyRateString.Equals(vacancyPagejavaVacancyRate.ToString()))
+        {
+            Console.WriteLine($"Number of vacancies on the home page({homePageJavaVacancyRateString}) is equal to the number of vacancies on the Vacancies tab({vacancyPagejavaVacancyRate})");
+        }
 
         driver.Close();            
     }
